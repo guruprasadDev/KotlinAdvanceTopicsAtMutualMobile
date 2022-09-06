a4555b101243e23beb3eddedc3c57b6fa758d4e9
guruprasadesambattu <guruprasad.esambattu@mutualmobile.com>
2022-09-06 19:31:20 +0530

    initial Commit

--- src/main/kotlin/InfixFunctionInKotlin.kt
+++ src/main/kotlin/InfixFunctionInKotlin.kt
@@ -7,4 +7,16 @@ It must accept a single parameter
 The parameter must not accept variable number of arguments and must have no default value
 It must be marked with infix keyword
 
+
  */
+
+fun main(){
+
+    val a =100 add(2000)
+
+    println(a)
+
+
+}
+
+infix fun Int.add(b:Int) = this + b

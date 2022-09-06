dbb8c5b5b86ae5bb6b0e57661738f4a935081b0e
guruprasadesambattu <guruprasad.esambattu@mutualmobile.com>
2022-09-06 14:18:00 +0530

    initial Commit

---
+++ src/main/kotlin/GenericMethodInKotlin.kt
@@ -0,0 +1,19 @@
+fun  main(){
+
+    val arrayListString : ArrayList<String> = arrayListOf("Guru","Vishnu")
+    val s :String = arrayListString[0]
+    println("ArrayList string value $s")
+    printValue(arrayListString)
+
+    val arrayListFloat : ArrayList<Float> = arrayListOf(100f,300f,400.0f)
+    printValue(arrayListFloat)
+
+}
+
+fun <T> printValue(list: ArrayList<T>) {
+
+    for(generics in list){
+        println(generics)
+    }
+
+}

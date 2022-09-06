5557973ab6f0010ece0c2d60367262b89fc3bf1a
guruprasadesambattu <guruprasad.esambattu@mutualmobile.com>
2022-09-06 17:55:24 +0530

    initial Commit

---
+++ src/main/kotlin/InlineFunctionInKotlin.kt
@@ -0,0 +1,27 @@
+/**
+  Inline Function
+
+ -> Inline function declare with a keyword called Inline
+ -> The use of inline function enhances the performance of higher order function
+ -> the inline function tells the compiler to copy the parameter and function to the call site
+
+ */
+
+fun main(){
+
+    inlineFunction { println("Calling the inline function") }
+
+}
+
+inline fun inlineFunction(fu: ()-> Unit){
+    fu()
+    println("Code inside the inline function")
+}
+
+/**
+ What is the difference between normal function and Inline function ?
+
+ So Basically what kotlin does  internally creates separate  Instance and class for every lambda we have created
+
+ the inline function tells the compiler to copy the parameter and function to the call site
+ */

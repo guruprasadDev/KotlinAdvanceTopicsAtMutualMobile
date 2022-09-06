4ea616842352f3b3989674459e4b90ba039a9be9
guruprasadesambattu <guruprasad.esambattu@mutualmobile.com>
2022-09-06 19:14:52 +0530

    initial Commit

---
+++ src/main/kotlin/InfixFunctionInKotlin.kt
@@ -0,0 +1,10 @@
+/**
+ *
+User defined infix function notation :-
+
+It must be member function or extension function
+It must accept a single parameter
+The parameter must not accept variable number of arguments and must have no default value
+It must be marked with infix keyword
+
+ */

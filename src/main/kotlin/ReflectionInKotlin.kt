44041b0c4f3a27c9e32ea65e0a4e9d8139eef319
guruprasadesambattu <guruprasad.esambattu@mutualmobile.com>
2022-09-06 15:56:32 +0530

    initial Commit

---
+++ src/main/kotlin/ReflectionInKotlin.kt
@@ -0,0 +1,18 @@
+
+class ReflectionInKotlin{
+
+}
+
+fun isEven(x:Int) = x%2==0
+
+fun main(){
+    val myList = listOf(1,2,3,4,5,6,7,8,9,10)
+    myList.filter(::isEven).forEach(::println)
+
+    val abc = ReflectionInKotlin::class
+    println("This a class reflection $abc")
+
+    val obj = ReflectionInKotlin()
+    println("these is bounded class reflection $obj")
+
+}
